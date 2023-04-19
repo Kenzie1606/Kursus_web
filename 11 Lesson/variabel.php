ca001953abd4084c1da2ac1fbb727ed1e1dad32b
Kenzie1606 <124428402+Kenzie1606@users.noreply.github.com>
2023-04-19 20:02:34 +0700

    Add files via upload

---
+++ 11 Lesson/variabel.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variable</title>
+</head>
+<body>
+    <h1>Variable:</h1>
+    <?php
+    /* $Name = "Lutfi<br>"; //string: text/alphabet/number/symbol inside ""/''
+    echo $Name. "<br>";
+    $age = 15; //integer: number not decimal
+    echo $age. "<br>" ;
+    $Decimal = 0.5; //float : decimal
+    echo $Decimal. "<br>";
+    $boolean = true; #true/false
+    echo $boolean; */
+
+    $fname = "lutfi";
+    $lname = "Ahmad";
+
+    echo $fname. "  ".$lname;
+
+    ?> 
+
+</body>
+</html>
